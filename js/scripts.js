a7fa8968a80ce166506edbf6c2c77985262516ac
parkerbenoit <72704200+parkerbenoit@users.noreply.github.com>
2021-01-25 20:33:45 -0800

    some user interface works

--- js/scripts.js
+++ js/scripts.js
@@ -1,28 +1,38 @@
 //Business Logic:
 
-const add(number1, number2) {
+const add = function(number1, number2) {
   return number1 + number2;
 };
 
-const subtract(number1, number2) {
+const subtract = function(number1, number2) {
   return number1 - number2;
 };
 
-const multiply(number1, number2) {
+const multiply = function(number1, number2) {
   return number1 * number2;
 };
 
-const divide(number1, number2) {
+const divide = function(number1, number2) {
   return number1 / number2;
 };
 
 // User Interface Logic
 
 $(document).ready(function() {
-  const number1= parseInt(prompt("Enter a number:"));
-  const number2= parseInt(prompt("Enter another number:"));
-
-  alert(add(number1, number2));
+  $("form#add").submit(function(event) {
+    event.preventDefault();
+    const number1 = parseInt($("#add1").val());
+    const number2 = parseInt($("#add2").val());
+    const result = add(number1, number2);
+    $("#output").text(result);
+  });
+  $("form#subtract").submit(function(event) {
+    event.preventDefault();
+    const number1 = parseInt($("#subtract1").val());
+    const number2 = parseInt($("#subtract2").val());
+    const result = subtract(number1, number2);
+    $("#output").text(result);
+  });
 });
 
 
