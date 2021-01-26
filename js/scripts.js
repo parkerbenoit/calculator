2884e43dc876cc36bd682c828f411ff13d1a95e5
parkerbenoit <72704200+parkerbenoit@users.noreply.github.com>
2021-01-25 19:52:36 -0800

    serperate business and UI logic

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,29 @@
-function add (number1, number2) {
-  return number1 + number2;
-}
+//Business Logic:
 
-const number1 = parseInt(prompt("Enter a number:")); 
-const number2 = parseInt(prompt("Enter another number:"));
-const result = add(number1, number2);
-alert(result);
+const add(number1, number2) {
+  return number1 + number2;
+};
+
+const subtract(number1, number2) {
+  return number1 - number2;
+};
+
+const multiply(number1, number2) {
+  return number1 * number2;
+};
+
+const divide(number1, number2) {
+  return number1 / number2;
+};
+
+// User Interface Logic
+
+$(document).ready(function() {
+  const number1= parseInt(prompt("Enter a number:"));
+  const number2= parseInt(prompt("Enter another number:"));
+
+  alert(add(number1, number2));
+});
+
+
+
